8bc5e03658cd5b0342e39d84e4332028acbf303a
ulldecorb <ausias.bertran@gmail.com>
2021-11-12 12:47:00 +0100

    Create trash section

--- main.js
+++ main.js
@@ -130,6 +130,10 @@ function cloneIcon(idToClone) {
         return cloneIcon;
 }
 
+function saveDiv(divId, title) {
+    doc.fromHTML(`<html><head><title>${title}</title></head><body>` + document.getElementById(divId).innerHTML + `</body></html>`);
+    doc.save('div.pdf');
+}
 
 // function end(e){
 // 	e.target.style.opacity = ''; // Pone la opacidad del elemento a 1 			
