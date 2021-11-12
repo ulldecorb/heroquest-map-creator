9796ddd819f5963cf36faf7ac6424a1a301c232a
ulldecorb <ausias.bertran@gmail.com>
2021-11-12 14:41:53 +0100

    Create savepdf funtion

--- main.js
+++ main.js
@@ -130,10 +130,34 @@ function cloneIcon(idToClone) {
         return cloneIcon;
 }
 
-function saveDiv(divId, title) {
+function savePDF(divId, title) {
     doc.fromHTML(`<html><head><title>${title}</title></head><body>` + document.getElementById(divId).innerHTML + `</body></html>`);
     doc.save('div.pdf');
 }
+
+function printDiv(divId,
+    title) {
+  
+    let mywindow = window.open('', 'PRINT', 'height=650,width=900,top=100,left=150');
+  
+    mywindow.document.write(`<html><head><title>${title}</title>`);
+    mywindow.document.write('</head><body >');
+    mywindow.document.write(document.getElementById(divId).innerHTML);
+    mywindow.document.write('</body></html>');
+  
+    mywindow.document.close(); // necessary for IE >= 10
+    mywindow.focus(); // necessary for IE >= 10*/
+  
+    mywindow.print();
+    mywindow.close();
+  
+    return true;
+}
+
+function printTest() {
+    var c = document.getElementById('map');
+    var t = c.getContext('2d');
+    window.open('', t.toDataURL());}
 
 // function end(e){
 // 	e.target.style.opacity = ''; // Pone la opacidad del elemento a 1 			
